cd3dd30fa44bc982d95f83df1c06247cf6bc9751
Sayed Afif <sayedafif@gmail.com>
2021-07-13 15:41:16 -0400

    Added Components and Artists

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [artist, setArtist] =  useState();
+  const [artistID, setArtistID] =  useState();
+  useEffect(() => {
+      async function getAlbums(){
+        const result = await axios.get(`https://itunes.apple.com/lookup?id=${ artistID }&entity=album`); 
+        console.log(result);
+      }
+      getAlbums();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h1>Music Project</h1>
+        <select value={artist} onChange={(event) =>{
+          setArtist(event.target.options[event.target.selectedIndex].text);
+          setArtistID(event.target.value);
+        }}>
+          <option value = "453523728" label = "Prateek Kuhad"> Prateek Kuhad </option>
+          <option value = "2479756766" label = "The Weeknd"> The Weeknd </option>
+          <option value = "471744" label = "Coldplay"> Coldplay </option>
+        </select>
+
+        <h3>Your favorite Artist is {artist} </h3>
+
     </div>
   );
 }
---
+++ src/components/Album/album.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function album(){
+    <div className = "albumCard">
+        <div className = "albumImage">
+        </div>
+        <div className = "albumName">
+
+        </div>
+
+    </div>
+}
